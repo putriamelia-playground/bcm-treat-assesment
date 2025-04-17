d5918308dc48996e7d47d71b78d11a28a01e2b70
putri.amelia <bcmputriamelia@gmail.com>
2025-04-17 21:43:24 +0700

    closes #132 #133 #134 #135

---
+++ database/migrations/2025_04_17_133637_update_users_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('assessor_company')->after('email')->nullable();
+            $table->string('assessor_company_code')->after('assessor_company')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('assessor_company');
+            $table->dropColumn('assessor_company_code');
+        });
+    }
+};
